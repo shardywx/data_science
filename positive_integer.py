6fbcb6023dc74fc47f37ad1afbea6509a682dbd2
Sam F Hardy <s.hardy1@leeds.ac.uk>
2023-07-11 17:20:56 +0100

    remove minor typos

--- positive_integer.py
+++ positive_integer.py
@@ -1,13 +1,13 @@
 def solution(A):
-"""
-Find the smallest positive integer that doesn't exist in an array 
-"""
+    """
+    Find the smallest positive integer that doesn't exist in an array 
+    """
     found = False
     n = len(A)
     for i in range(1, n+2):
         found = False 
         for j in range(n):
-            if a[j] == i:
+            if A[j] == i:
                 found = True 
                 break
         if found == False:
