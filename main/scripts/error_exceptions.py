aa160c60a638df628c614fa39edd8623dad2fdbd
Sam F Hardy <s.hardy1@leeds.ac.uk>
2023-08-18 13:18:58 +0100

    error exceptions exercise

--- main/scripts/error_exceptions.py
+++ main/scripts/error_exceptions.py
@@ -1,5 +1,8 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 # Problem defined here: https://www.hackerrank.com/challenges/exceptions/problem
+# Task: You are given two values 'a' and 'b'. Perform integer division and print a/b. 
+# The first line of input from stdin contains T, the number of test cases. The next T lines each contain
+# the space separated values of a and b. 
 
 def exceptions(a, b):
 
